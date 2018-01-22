b3817266adaef64fb75c9f3d9ca1c87c0b58d879
Yomaira Escano <yjescano@gmail.com>
2018-01-22 13:22:49 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
